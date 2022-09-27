22d39fd69c54f1dc4a042e8a52ab6a796fca517c
ImaGe0401 <fesespecialist@gmail.com>
2022-09-28 03:59:00 +0900

    update

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -1,6 +1,11 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, web, App, HttpResponse, HttpServer, ResponseError};
 use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+//use r2d2_sqlite::rusqlite;
+use rusqlite::params;
 use thiserror::Error;
+
 
 struct TodoEntry{
     id: u32,
@@ -15,32 +20,56 @@ struct IndexTemplate{
 #[derive(Error, Debug)]
 enum MyError{
     #[error("Failed to render HTML")]
-    AskamaError(#[from] askama::Error)
+    AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError>{
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError>{
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM todo")?;
+    let rows = statement.query_map(params![], |row|{
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(TodoEntry { id, text})
+    })?;
+
     let mut entries = Vec::new();
-    entries.push(TodoEntry{
-        id: 1,
-        text: "first entry".to_string(),
-    });
-    entries.push(TodoEntry{
-        id: 2,
-        text: "second entry".to_string(),
-    });
+
+    for row in rows{
+        entries.push(row?);
+    }
     let html = IndexTemplate { entries };
     let response_body = html.render()?;
     Ok(HttpResponse::Ok()
         .content_type("text/html")
         .body(response_body))
 }
-
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS todo(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("Failed to create a table 'todo'.");
+    HttpServer::new(move || App::new().service(index).data(pool.clone()))
+    //HttpServer::new(move || .app_data(Data::new(pool)))
         .bind("0.0.0.0:8080")?
         .run()
         .await?;
